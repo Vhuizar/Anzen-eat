12baf981b4191a0d6a4b45e06f0df0df1355f029
Vhuizar <jaquelinehuizar@hotmail.com>
2020-08-22 23:04:30 -0500

    cambios aun el bd

--- src/index.js
+++ src/index.js
@@ -36,8 +36,8 @@ app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 
 //variables globales
 app.use((req, res, next) =>{
