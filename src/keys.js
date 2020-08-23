ebf5b3b61e96a98cf6d245702e080069f47ed887
Vhuizar <jaquelinehuizar@hotmail.com>
2020-08-22 22:31:22 -0500

    cambios en la bd

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,5 @@
 module.exports = {
     database: {
-        connectionLimit : 1000,
-        connectTimeout  : 60 * 60 * 1000,
-        acquireTimeout  : 60 * 60 * 1000,
-        timeout         : 60 * 60 * 1000,
         host: 'localhost',
         port: 8080,
         user: 'Anzen-eat',
