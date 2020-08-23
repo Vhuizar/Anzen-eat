7a5de478dd4c333d4c0e8ddd11ef57e4787a43b9
Vhuizar <jaquelinehuizar@hotmail.com>
2020-08-22 22:22:24 -0500

    bd Conflictos

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,9 @@
 module.exports = {
     database: {
+        connectionLimit : 1000,
+        connectTimeout  : 60 * 60 * 1000,
+        acquireTimeout  : 60 * 60 * 1000,
+        timeout         : 60 * 60 * 1000,
         host: 'localhost',
         port: 8080,
         user: 'Anzen-eat',
